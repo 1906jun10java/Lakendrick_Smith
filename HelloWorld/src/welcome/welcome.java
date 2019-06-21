acaae9f777bc89cff05d2b9685fcdba5bc334bf7
Lakendrick26 <lakendrick53@gmail.com>
2019-06-20 20:01:22 -0500

    test

---
+++ HelloWorld/src/welcome/welcome.java
@@ -0,0 +1,21 @@
+package welcome;
+
+public class  welcome {
+
+	public static void main(String args[]) {
+		
+		for(int x = 100; x > 20; x = x - 1) {
+			
+			System.out.println("value of x : " + x);
+			
+			System.out.println("\n");
+			
+		}
+		
+		
+		
+		
+	}
+	
+
+}
