69fa423a556b9591d5ae93d947b54bbc67708e1a
Lakendrick26 <lakendrick53@gmail.com>
2019-07-15 14:16:44 -0500

    week3_javascript_challenge

---
+++ week3_challenge/codeChallenge.js
@@ -0,0 +1,16 @@
+var Lord_Of_The_Rings = {
+    Name: "Lord Of The Rings",
+    Author: "J. R. R. Tolkien",
+    Genre: "Fantasy",
+    image: undefined
+};
+
+var Tears_Of_A_Tiger = {
+    Name: "Tears_Of_A_Tiger",
+    Author: " Sharon Draper",
+    Genre: "Drama/Tragedy",
+    image: undefined
+
+};
+
+var libray = [Lord_Of_The_Rings, Tears_Of_A_Tiger];
